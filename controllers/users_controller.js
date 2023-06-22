e3104d35ad1d42728ee084260b66bcda0b660420
Abhijit Roy <abhiroy67981@gmail.com>
2023-06-22 23:05:31 +0530

    Authentication steps

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,50 @@
+const User = require('../models/user');
+
 module.exports.profile= function(req,res){
     return res.render('user_profile',{
         title: "User Profile"
     });
+}
+
+
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title: "Codeial | Sign Up"
+    });
+}
+
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title: "Codeial | Sign In"
+    });
+}
+
+
+//get the sign up data
+module.exports.create = function(req,res){
+   if(req.body.password != req.body.confirm_password){
+    return res.redirect('back');
+   }
+   
+User.findOne({email: req.body.email}, function(err,user){
+    if(err){
+        console.log('Error in Finding user in Signing Up');
+        return }
+    if(!user){
+        User.create(req.body, function(err,user){
+            if(err){console.log('Error in creating user while Signing Up');
+            return}
+
+            return res.redirect('/users/sign-in');
+    })
+    }else{
+        return res.redirect('back');
+    }
+});
+}
+
+
+//create session and sign in with user data
+module.exports.createSession = function(req,res){
+    //to do later
 }
